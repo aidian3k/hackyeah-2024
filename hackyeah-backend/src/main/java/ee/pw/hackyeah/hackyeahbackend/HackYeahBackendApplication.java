d39fc920c0a34036029824da52d93d75523ade92
Adrian Nowosielski <adrian.nowosielski1@gmail.com>
2024-09-28 10:20:30 +0200

    added files

---
+++ hackyeah-backend/src/main/java/ee/pw/hackyeah/hackyeahbackend/HackYeahBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.pw.hackyeah.hackyeahbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackYeahBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackYeahBackendApplication.class, args);
+	}
+
+}
---
+++ hackyeah-backend/src/test/java/ee/pw/hackyeah/hackyeahbackend/HackYeahBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.pw.hackyeah.hackyeahbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackYeahBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
