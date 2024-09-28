19b62f597a1e16d181ce612d38c8812ba7002aa7
dobdaw <doboszdav@gmail.com>
2024-09-28 23:10:04 +0200

    fixes

--- hackyeah-frontend/src/router/Routes.component.tsx
+++ hackyeah-frontend/src/router/Routes.component.tsx
@@ -1,10 +1,9 @@
-import React, { FC } from 'react';
-import { Route } from 'react-router-dom';
-import { RoutePaths } from './Routes.types';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import React, {FC} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {RoutePaths} from './Routes.types';
 import LoginPage from '@/pages/Login/LoginPage.component';
-import { RootState } from '@/store/store';
-import { useSelector } from 'react-redux';
+import {RootState} from '@/store/store';
+import {useSelector} from 'react-redux';
 import RegisterPage from '@/pages/Register/RegisterPage.component';
 import AuthorizedLayout from '@/features/AuthorizedLayout.component.tsx';
 import HomePage from '@/pages/home-page/home-page.tsx';
@@ -20,8 +19,8 @@ const AppRoutes: FC = () => {
           <Route path={RoutePaths.LOGIN} element={<LoginPage />} />
           <Route path={RoutePaths.REGISTER} element={<RegisterPage />} />
         </Route>
-        <Route path="/" element={<AuthorizedLayout />}>
           <Route path={RoutePaths.MAIN_PAGE} element={<HomePage />} />
+        <Route path="/" element={<AuthorizedLayout />}>
           <Route path={RoutePaths.DASHBOARD} element={<Dashboard />} />
         </Route>
       </Routes>
