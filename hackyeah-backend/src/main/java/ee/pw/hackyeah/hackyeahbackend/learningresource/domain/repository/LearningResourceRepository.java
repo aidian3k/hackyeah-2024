85c57628fc04f919bba057815d3d572c75903ab3
Adrian Nowosielski <adrian.nowosielski1@gmail.com>
2024-09-29 07:25:48 +0200

    Fixed repository

--- hackyeah-backend/src/main/java/ee/pw/hackyeah/hackyeahbackend/learningresource/domain/repository/LearningResourceRepository.java
+++ hackyeah-backend/src/main/java/ee/pw/hackyeah/hackyeahbackend/learningresource/domain/repository/LearningResourceRepository.java
@@ -3,11 +3,18 @@ package ee.pw.hackyeah.hackyeahbackend.learningresource.domain.repository;
 import ee.pw.hackyeah.hackyeahbackend.learningresource.domain.LearningResource;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LearningResourceRepository
     extends JpaRepository<LearningResource, Long> {
+
+    @Query("select lr from LearningResource lr" +
+            " where lr.subject.name like %:subjectName%" +
+            " and lr.subject.course.id = :courseId " +
+            "and lr.subject.course.faculty.id = :unitId " +
+            "and lr.subject.course.faculty.institution.id = :institutionId")
     Set<LearningResource> findLearningResourcesBySubjectNameAndCourseIdAndInstitutionIdAndUnitId(
         String subjectName,
         String courseId,
