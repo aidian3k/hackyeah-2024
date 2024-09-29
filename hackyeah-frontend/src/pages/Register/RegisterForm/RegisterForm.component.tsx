df3c1ba0b620043b0d4ab54f611292f0d63e4535
dobdaw <doboszdav@gmail.com>
2024-09-29 08:16:16 +0200

    login + register

--- hackyeah-frontend/src/pages/Register/RegisterForm/RegisterForm.component.tsx
+++ hackyeah-frontend/src/pages/Register/RegisterForm/RegisterForm.component.tsx
@@ -1,92 +1,82 @@
-import { useRegisterUser } from '@/api/mutation/RegisterMutation';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { RoutePaths } from '@/router/Routes.types';
-import { RegisterFormInputs } from '@/ts/interface/Register.types';
-import React, { useEffect } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import {useRegisterUser} from '@/api/mutation/RegisterMutation';
+import {Button} from '@/components/ui/button';
+import {Input} from '@/components/ui/input';
+import {Label} from '@/components/ui/label';
+import {RoutePaths} from '@/router/Routes.types';
+import {RegisterFormInputs} from '@/ts/interface/Register.types';
+import React, {useEffect} from 'react';
+import {SubmitHandler, useForm} from 'react-hook-form';
+import {useNavigate} from 'react-router-dom';
 
 const RegisterForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<RegisterFormInputs>();
-  const { mutate: handleRegister, isSuccess, isError, error } = useRegisterUser();
-  const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm<RegisterFormInputs>();
+    const {mutate: handleRegister, isSuccess, isError, error} = useRegisterUser();
+    const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<RegisterFormInputs> = data => {
-    console.log(data);
-    handleRegister(data);
-  };
+    const onSubmit: SubmitHandler<RegisterFormInputs> = data => {
+        console.log(data);
+        handleRegister(data);
+    };
 
-  useEffect(() => {
-    if (isSuccess) {
-      console.log('User registered');
-      navigate(RoutePaths.LOGIN);
-    }
-  }, [isSuccess]);
+    useEffect(() => {
+        if (isSuccess) {
+            console.log('User registered');
+            navigate(RoutePaths.LOGIN);
+        }
+    }, [isSuccess]);
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-      <div>
-        <Label htmlFor="email">Email:</Label>
-        <Input type="email" id="email" {...register('email', { required: 'Email jest wymagany' })} />
-        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-      </div>
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+                <Label htmlFor="email">Email:</Label>
+                <Input type="email" id="email" {...register('email', {required: 'Email jest wymagany'})} />
+                {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+            </div>
 
-      <div>
-        <Label htmlFor="password">Hasło:</Label>
-        <Input type="password" id="password" {...register('password', { required: 'Hasło jest wymagane' })} />
-        {errors.password && <p className="text-red-500">{errors.password.message}</p>}
-      </div>
+            <div>
+                <Label htmlFor="password">Hasło:</Label>
+                <Input type="password" id="password" {...register('password', {required: 'Hasło jest wymagane'})} />
+                {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+            </div>
 
-      <div>
-        <Label htmlFor="password">Powtórz hasło:</Label>
-        <Input type="password" id="confirm-password" {...register('confirmPassword', { required: 'Potwierdź wprowadzone hasło' })} />
-        {errors.password && <p className="text-red-500">{errors.password.message}</p>}
-      </div>
+            <div>
+                <Label htmlFor="password">Powtórz hasło:</Label>
+                <Input type="password"
+                       id="confirm-password" {...register('confirmPassword', {required: 'Potwierdź wprowadzone hasło'})} />
+                {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+            </div>
 
-      <div>
-        <Label htmlFor="firstName">Imię:</Label>
-        <Input type="text" id="firstName" {...register('firstName', { required: 'Imię jest wymagane' })} />
-        {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
-      </div>
+            <div>
+                <Label htmlFor="firstName">Imię:</Label>
+                <Input type="text" id="firstName" {...register('firstName', {required: 'Imię jest wymagane'})} />
+                {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
+            </div>
 
-      <div>
-        <Label htmlFor="lastName">Nazwisko (opcjonalnie):</Label>
-        <Input type="text" id="lastName" {...register('lastName')} />
-      </div>
+            <div>
+                <Label htmlFor="lastName">Nazwisko (opcjonalnie):</Label>
+                <Input type="text" id="lastName" {...register('lastName')} />
+            </div>
 
-      <div>
-        <Label htmlFor="nickName">Nazwa użytkownika (nick):</Label>
-        <Input type="text" id="nickName" {...register('nickName', { required: 'Nick jest wymagany' })} />
-        {errors.nickName && <p className="text-red-500">{errors.nickName.message}</p>}
-      </div>
+            <div>
+                <Label htmlFor="nickName">Nazwa użytkownika (nick):</Label>
+                <Input type="text" id="nickName" {...register('nickName', {required: 'Nick jest wymagany'})} />
+                {errors.nickName && <p className="text-red-500">{errors.nickName.message}</p>}
+            </div>
 
-      <div>
-        <Label htmlFor="phoneNumber">Numer telefonu:</Label>
-        <Input type="tel" id="phoneNumber" {...register('phoneNumber', { required: 'Numer telefonu jest wymagany' })} />
-        {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber.message}</p>}
-      </div>
+            <div>
+                <Label htmlFor="phoneNumber">Numer telefonu:</Label>
+                <Input type="tel"
+                       id="phoneNumber" {...register('phoneNumber', {required: 'Numer telefonu jest wymagany'})} />
+                {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber.message}</p>}
+            </div>
 
-      <div>
-        <Label htmlFor="birthDate">Data urodzenia:</Label>
-        <Input type="date" id="birthDate" {...register('birthDate', { required: 'Data urodzenia jest wymagana' })} />
-        {errors.birthDate && <p className="text-red-500">{errors.birthDate.message}</p>}
-      </div>
-
-      <div>
-        <Label htmlFor="organization">Organizacja:</Label>
-        <Input type="text" id="organization" {...register('organization', { required: 'Organizacja jest wymagana' })} />
-        {errors.organization && <p className="text-red-500">{errors.organization.message}</p>}
-      </div>
-
-      <Button type="submit">Zarejestruj się</Button>
-    </form>
-  );
+            <Button type="submit">Zarejestruj się</Button>
+        </form>
+    );
 };
 
 export default RegisterForm;
